bb77eed3690179cc1f2d7b352b518e5f0c8fd81e
vNemetz <vininemetz@gmail.com>
2026-03-28 23:19:37 -0300

    feat: synth audio source

---
+++ src/AudioSource/SynthAudioSource.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <juce_core/juce_core.h>
+#include <juce_events/juce_events.h>
+#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_devices/juce_audio_devices.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+
+class SynthAudioSource : public juce::AudioIODeviceCallback{
+private:
+	double currentPhase = 0.0f;
+  double phaseIncrement = 0.0f;
+	
+public: 
+	void audioDeviceIOCallbackWithContext (
+    const float* const* inputChannelData,
+		int numInputChannels,
+	  float* const* outputChannelData,
+		int numOutputChannels,
+		int numSamples,
+		const juce::AudioIODeviceCallbackContext& context) override;
+
+	void audioDeviceAboutToStart (juce::AudioIODevice* device) override;
+	void audioDeviceStopped() override;
+
+};
